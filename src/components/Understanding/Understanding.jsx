1d4744e8f37f29b1b9e57b11d3f6ddfdc3eac085
Alex White <alexlloydwhite@gmail.com>
2021-05-07 14:35:24 -0500

    understanding form setup, dispatched

--- src/components/Understanding/Understanding.jsx
+++ src/components/Understanding/Understanding.jsx
@@ -1,6 +1,30 @@
+import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import {useDispatch} from 'react-redux';
+
 const Understanding = () => {
+    // local state for feelings
+    const [understand, setUnderstand] = useState('');
+    // init for dispatch
+    const dispatch = useDispatch();
+
+    const history = useHistory();
+
+    const submit = () => {
+        // dispatch to reducer
+        dispatch({
+            type: 'SET_UNDERSTANDING',
+            payload: understand
+        });
+        // on click next => next page
+        history.push('/support');
+    }
+
     return (  
-        <h1>understanding goes here</h1>
+        <form onSubmit={submit}>
+            <input onChange={(event) => setUnderstand(event.target.value)} type="text" placeholder="Understanding?" value={understand} />
+            <button>Next</button>
+        </form>
     );
 }
  
