86aa998b2639f38338abe270493bf65c45f881c3
Alex White <alexlloydwhite@gmail.com>
2021-05-07 16:12:13 -0500

    review page conditional rending complete

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,27 +1,37 @@
 import { useSelector } from 'react-redux';
-import axios from 'axios';
+import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
 const Review = () => {
 
     const feedback = useSelector(store => store.feedbackReducer);
+    const [toggled, setToggled] = useState(true);
 
+    const history = useHistory();
     const submit = () => {
-        axios({
-            method: 'POST',
-            url: ''
-        })
+        setToggled(false);
     }
 
-    return (  
-        <div>
-            <h2>Review Your Feedback</h2>
-            <p>Feelings: {feedback.feeling}</p>
-            <p>Understanding: {feedback.understanding}</p>
-            <p>Support: {feedback.support}</p>
-            <p>Comments: {feedback.comments}</p>
-            <button onClick={submit}>Submit</button>
-        </div>
-    );
+    if (toggled) {
+        return (
+            <div>
+                <h2>Review Your Feedback</h2>
+                <p>Feelings: {feedback.feeling}</p>
+                <p>Understanding: {feedback.understanding}</p>
+                <p>Support: {feedback.support}</p>
+                <p>Comments: {feedback.comments}</p>
+                <button onClick={submit}>Submit</button>
+            </div>
+        );
+    } else {
+        return (
+            <div>
+                <h2>Thank You!</h2>
+                <button onClick={() => history.push('/')}>Submit New Feedback</button>
+            </div>
+        )
+    }
+
 }
- 
+
 export default Review;
