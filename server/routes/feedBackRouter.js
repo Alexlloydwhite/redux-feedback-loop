037cd4b85404f99a7677b37889d80ce4389643da
Alex White <alexlloydwhite@gmail.com>
2021-05-09 15:34:59 -0500

    setting up server to post data to postgres

---
+++ server/routes/feedBackRouter.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const pool = require('../modules/pool');
+
+router.post('/', (req, res) => {
+    // assigning req body for reabability 
+    const feedback = req.body;
+    // sql query to send to db
+    const sqlText = `INSERT INTO feedback(feeling, understanding, support, comments)
+                        VALUES($1,$2,$2,$4);`;
+    // Sending query to the db
+    pool.query(sqlText, [feedback.feeling, feedback.understanding, feedback.support, feedback.comments])
+        .then(result => {
+            console.log('added feedback to db:', feedback);
+            res.sendStatus(201);
+        })
+        .catch( error => {
+            console.log(`error making db query ${sqlText}`, error);
+            res.sendStatus(500);
+        })
+})
+
+module.exports = router;
